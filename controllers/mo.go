086de8b8ebe456f495e2c488fe36f95706084af8
Samuel Sekiwere <sekiskylink@gmail.com>
2023-02-23 13:58:09 +0300

    all post json requests from nitau server

--- controllers/mo.go
+++ controllers/mo.go
@@ -12,45 +12,60 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type PostBody struct {
+	PhoneNumber string `form:"phone_number" json:"phone_number"`
+	Text        string `form:"text" json:"text"`
+	ShortCode   string `form:"short_code" json:"short_code"`
+	tstamp      string `form:"tstamp" json:"tstamp"`
+}
+
 // MOController will hold the methods to
 type MOController struct{}
 
 // Default controller handles returning the SMS JSON response
 func (m *MOController) Default(c *gin.Context) {
-	from := c.Query("phone_number")
-	shortCode := c.Query("short_code")
-	text := c.Query("text")
-	fmt.Printf("From: %s, ShortCode: %s, Text: %s\n", from, shortCode, text)
-	log.Println("Received SMS: From:%s, To:%s, [Msg: %s]", from, shortCode, text)
+	// from := c.Query("phone_number")
+	// shortCode := c.Query("short_code")
+	// text := c.Query("text")
+	var postBody PostBody
 
-	apiURL, err := url.Parse(helpers.GetDefaultEnv("NITAU_API_MO_URL", "http://localhost:8000/?"))
+	if c.ShouldBind(&postBody) == nil {
+		fmt.Printf(
+			"From: %v, ShortCode: %v, Text: %v\n",
+			postBody.PhoneNumber, postBody.ShortCode, postBody.Text)
+		log.Println(
+			"Received SMS: From:%v, To:%v, [Msg: %v]",
+			postBody.PhoneNumber, postBody.ShortCode, postBody.Text)
 
-	data := url.Values{}
-	data.Set("from", from)
-	data.Set("text", text)
-	apiURL.RawQuery = data.Encode()
+		apiURL, err := url.Parse(helpers.GetDefaultEnv("NITAU_API_MO_URL", "http://localhost:8000/?"))
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
+		data := url.Values{}
+		data.Set("from", postBody.PhoneNumber)
+		data.Set("text", postBody.Text)
+		apiURL.RawQuery = data.Encode()
 
-	client := &http.Client{Transport: tr}
-	r, _ := http.NewRequest(http.MethodPost, apiURL.String(), nil) // URL-encoded payload
-	// r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	// r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-
-	resp, err := client.Do(r)
-	if err != nil && resp == nil {
-		log.Fatalf("Error sending request to RapidPro MO URL. %+v", err)
-	} else {
-		fmt.Println(resp.Status)
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalf("Couldn't parse response body. %+v", err)
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
 
-		log.Println("Response Body:", string(body))
+		client := &http.Client{Transport: tr}
+		r, _ := http.NewRequest(http.MethodPost, apiURL.String(), nil) // URL-encoded payload
+		// r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		// r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+		resp, err := client.Do(r)
+		if err != nil && resp == nil {
+			log.Fatalf("Error sending request to RapidPro MO URL. %+v", err)
+		} else {
+			fmt.Println(resp.Status)
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("Couldn't parse response body. %+v", err)
+			}
+
+			log.Println("Response Body:", string(body))
+		}
 	}
 
 	c.JSON(200, gin.H{"message": "Received SMS"})
